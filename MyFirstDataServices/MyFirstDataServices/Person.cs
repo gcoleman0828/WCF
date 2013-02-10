1e734e2f767e24375ee99382e04f0fe208fd4d3f
gcoleman0828 <Gregg.m.coleman@gmail.com>
2013-02-10 00:19:04 -0500

    Adding First Data Service Tutorial to follow blog
    Data Service Tutorial code to follow post
    http://objectreferncehell.wordpress.com/2013/02/10/wcf-data-services-odata-consumption/

---
+++ MyFirstDataServices/MyFirstDataServices/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Services.Common;
+using System.Linq;
+using System.Web;
+
+namespace MyFirstDataServices
+{
+    [DataServiceKey("Id")]
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Title { get; set; }
+
+    }
+}
---
+++ MyFirstDataServices/MyFirstDataServices/PersonContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyFirstDataServices
+{
+    public class PersonContext
+    {
+        private static List<Person> _personList;
+
+        public IQueryable<Person> Persons { get { return _personList.AsQueryable(); } }
+
+
+        public PersonContext()
+        {
+            _personList = GetPersonsData();
+        }
+
+        public List<Person> GetPersonsData()
+        {
+
+            List<Person> persons = new List<Person>();
+            persons.Add(new Person()
+            {
+                ID = 1,
+                Name = "Gregg",
+                Age = 31,
+                Title = "Software Engineer"
+
+            });
+
+            persons.Add(new Person()
+            {
+                ID = 2,
+                Name = "George",
+                Age = 38,
+                Title = "Software Engineer"
+
+            });
+
+            persons.Add(new Person()
+            {
+                ID = 3,
+                Name = "Rachel",
+                Age = 21,
+                Title = "Waitress"
+
+            });
+
+            return persons;
+
+        }
+
+    }
+}
